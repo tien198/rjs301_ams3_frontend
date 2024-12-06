eefe655b2ccc9875bc8ee8429f26b1d163c8ab32
tien <tienvn998@gmail.com>
2024-12-06 16:31:40 +0700

    add LiveChatIcon

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -5,22 +5,26 @@ import LiveChatIcon from "../components/UI/LiveChatIcon";
 import { PageUrlsList } from "../ultil/ultilEnums";
 import { useEffect, useState } from "react";
 
+/**
+ * This function is passed a path need to check and an array of pathNames will apply LiveChat component
+ * @param checkingPath - current path need to check
+ * @param pathNamesList - array of pathNames will apply LiveChat component
+ */
+function isHasLiveChat(checkingPath: string, pathNamesList: string[]) {
+    for (let i of pathNamesList) {
+        if (checkingPath.includes(i))
+            return true
+    }
+    return false
+}
+
 function Root() {
     const location = useLocation()
     const [hasLiveChat, setHasLiveChat] = useState(false)
 
-
-
     useEffect(() => {
-        function isHasLiveChat(pathNamesList: string[]) {
-            for (let i of pathNamesList) {
-                if (location.pathname.includes(i))
-                    return true
-            }
-            return false
-        }
         if (location.pathname === PageUrlsList.Home
-            || isHasLiveChat([PageUrlsList.Shop, PageUrlsList.Cart])) {
+            || isHasLiveChat(location.pathname, [PageUrlsList.Shop, PageUrlsList.Cart])) {
             setHasLiveChat(true)
         }
         else setHasLiveChat(false)
